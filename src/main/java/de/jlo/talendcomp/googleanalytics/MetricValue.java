8ff22017a95ad5eab2ca5bd6df2ad6210e7d3779
Jan Lolling <jan.lolling@gmail.com>
2014-10-16 14:45:57 +0000

    release 3.0 package name changed normalized output flows for dimensions and metrics

---
+++ src/main/java/de/jlo/talendcomp/googleanalytics/MetricValue.java
@@ -0,0 +1,9 @@
+package de.jlo.talendcomp.googleanalytics;
+
+public class MetricValue {
+	
+	public String name;
+	public Double value;
+	public int rowNum;
+
+}
--- src/test/java/TestGoogleAnalytics.java
+++ src/test/java/TestGoogleAnalytics.java
@@ -1,6 +1,8 @@
 import java.util.List;
 
-import de.cimt.talendcomp.googleanalytics.GoogleAnalyticsInput;
+import de.jlo.talendcomp.googleanalytics.DimensionValue;
+import de.jlo.talendcomp.googleanalytics.GoogleAnalyticsInput;
+import de.jlo.talendcomp.googleanalytics.MetricValue;
 
 public class TestGoogleAnalytics {
 
@@ -17,12 +19,11 @@ public class TestGoogleAnalytics {
 		gi.setApplicationName("GATalendComp");
 
 		gi.setAccountEmail("503880615382@developer.gserviceaccount.com");
-		gi.setKeyFile("config/2bc309bb904201fcc6a443ff50a3d8aca9c0a12c-privatekey.p12");
+		gi.setKeyFile("/Volumes/Data/Talend/testdata/ga/config/2bc309bb904201fcc6a443ff50a3d8aca9c0a12c-privatekey.p12");
 		//gi.setAccountEmail("jan.lolling@gmail.com");
 		// gi.setProfileId("33360211"); // 01_main_profile
 		// gi.setAccountEmail("422451649636@developer.gserviceaccount.com");
 		// gi.setKeyFile("/home/jlolling/Documents/cimt/projects/mobile/GA_Service_Account/af21f07c84b14af09c18837c5a385f8252cc9439-privatekey.p12");
-		System.out.println("asldkasdljkasdlkasdlkasd");
 		gi.setFetchSize(20000);
 		
 		gi.setTimeoutInSeconds(240);
@@ -39,14 +40,14 @@ public class TestGoogleAnalytics {
 		// System.out.println("############################# " + i +
 		// " ########################");
 		gi.setProfileId("59815695");
-		gi.setStartDate("2012-12-15");
-		gi.setEndDate("2013-01-01");
+		gi.setStartDate("2014-01-01");
+		gi.setEndDate("2014-10-01");
 
 		gi.setDimensions("ga:date,ga:source,ga:keyword");
 		gi.setMetrics("ga:visits,ga:exits");
-		gi.setSorts("ga:date");
 		try {
-			fetchData(gi);
+//			fetchPlainData(gi);
+			fetchNormalizedData(gi);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -54,7 +55,64 @@ public class TestGoogleAnalytics {
 		}
 	}
 
-	private static void fetchData(GoogleAnalyticsInput gi) throws Exception {
+	private static void fetchNormalizedData(GoogleAnalyticsInput gi) throws Exception {
+		System.out.println("Start fetching first data...");
+		gi.executeQuery();
+		System.out.println("total results:" + gi.getTotalAffectedRows());
+		System.out.println("contains sampled data:" + gi.containsSampledData());
+		System.out.println("results:");
+		boolean firstLoop = true;
+		List<String> columnNames = gi.getColumnNames();
+		for (String name : columnNames) {
+			if (firstLoop) {
+				firstLoop = false;
+			} else {
+				System.out.print("\t|\t");
+			}
+			System.out.print(name);
+		}
+		System.out.println();
+		System.out.println("---------------------------------------");
+		List<String> columnTypes = gi.getColumnTypes();
+		firstLoop = true;
+		for (String type : columnTypes) {
+			if (firstLoop) {
+				firstLoop = false;
+			} else {
+				System.out.print("\t|\t");
+			}
+			System.out.print(type);
+		}
+		System.out.println();
+		System.out.println("---------------------------------------");
+		int index = 0;
+		int indexDimensionValue = 0;
+		int indexMetricValue = 0; 
+		while (true) {
+			try {
+				// in hasNext we execute query if needed
+				if (gi.nextNormalizedRecord() == false) {
+					break;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			}
+			index++;
+			DimensionValue dv = gi.getCurrentDimensionValue();
+			if (dv != null) {
+				indexDimensionValue++;
+				System.out.println("DM: rowNum=" + dv.rowNum + " dimension=" + dv.name + " value=" + dv.value);
+			}
+			MetricValue mv = gi.getCurrentMetricValue();
+			if (mv != null) {
+				indexMetricValue++;
+				System.out.println("MV: rowNum=" + mv.rowNum + " metric=" + mv.name + " value=" + mv.value);
+			}
+		}
+	}
+
+	private static void fetchPlainData(GoogleAnalyticsInput gi) throws Exception {
 		System.out.println("Start fetching first data...");
 		gi.executeQuery();
 		System.out.println("total results:" + gi.getTotalAffectedRows());
@@ -88,7 +146,7 @@ public class TestGoogleAnalytics {
 		while (true) {
 			try {
 				// in hasNext we execute query if needed
-				if (gi.hasNextDataset() == false) {
+				if (gi.hasNextPlainRecord() == false) {
 					break;
 				}
 			} catch (Exception e) {
@@ -96,7 +154,7 @@ public class TestGoogleAnalytics {
 				break;
 			}
 			index = gi.getCurrentIndexOverAll();
-			List<String> list = gi.getNextDataset();
+			List<String> list = gi.getNextPlainRecord();
 			System.out.print(index);
 			System.out.print("#\t");
 			firstLoop = true;
